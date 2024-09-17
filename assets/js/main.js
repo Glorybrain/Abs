f6d5ec2ac7939cb22383259fed6098ff08624265
Glorybrain <113359718+Glorybrain@users.noreply.github.com>
2024-09-17 18:10:02 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*=============== Add Box Shadow to Header ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('navbar')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*============== REVIEW SWIPER ===============*/
+var swiper = new Swiper(".review_slider",{
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
