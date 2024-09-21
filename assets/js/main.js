523cd3f810ec7cad373a21538d9123ccc1c78b99
Glorybrain <113359718+Glorybrain@users.noreply.github.com>
2024-09-21 14:42:33 +0100

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,22 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
 });
+
+//Countdown
+let valueDisplays = document.querySelectorAll('.num');
+let interval = 10000;
+
+valueDisplays.forEach((valueDisplays) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute('data-val'));
+    let duration = Math.floor(interval / endValue);
+
+    let counter = setInterval(function(){
+        startValue += 1;
+        valueDisplays.textContent = startValue;
+
+        if(startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
